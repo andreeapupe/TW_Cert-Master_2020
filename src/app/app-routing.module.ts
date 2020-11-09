d6521c93b0dc21db7df2744402863ca9646c0525
andreeapupe <andreea.pupe99@e-uvt.ro>
2020-11-09 19:09:01 +0200

    Implemented Login-SignUp Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginSignupComponent } from './LOGIN_SIGNUP/login-signup/login-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/welcome-screen',
+    pathMatch: 'full',
+  },
+  {
+    path: 'welcome-screen',
+    component: LoginSignupComponent,
+  },
+
+  { path: '**', component: LoginSignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
